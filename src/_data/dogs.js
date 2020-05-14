026e98c09b9adc283f3ea8bddc1881ccb9c484ed
Sia Karamalegos <siakaramalegos@gmail.com>
2020-05-14 15:24:34 -0500

    Add global dogs data and page

---
+++ src/_data/dogs.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    name: 'Harry',
+    breed: 'Jack Russell terrier',
+    favoritePasttime: 'finding hidey holes',
+    stinkLevel: 3,
+  },
+  {
+    name: 'Priscilla',
+    breed: 'Australian shepherd',
+    favoritePasttime: 'starting farty parties',
+    stinkLevel: 1,
+  }
+]
